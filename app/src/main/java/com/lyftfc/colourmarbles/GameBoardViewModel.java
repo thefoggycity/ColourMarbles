dbf88c5475567cbcd1aec9c546cee6ae1906c040
Li Yunfan <TheFoggyCity@live.cn>
2021-05-29 08:15:53 +0800

    Initial commit (GameBoard layout)

---
+++ app/src/main/java/com/lyftfc/colourmarbles/GameBoardViewModel.java
@@ -0,0 +1,7 @@
+package com.lyftfc.colourmarbles;
+
+import androidx.lifecycle.ViewModel;
+
+public class GameBoardViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/lyftfc/colourmarbles/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lyftfc.colourmarbles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
